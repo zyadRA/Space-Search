d32b0ac88cf71a38bd93cc0b176495a6c7424311
zyadRA <zyadmohmd009@gmail.com>
2025-05-04 20:40:10 +0300

    Added evaluation

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import random
 from SpaceEnvironment import SpaceEnvironment
+import time
 
 # initialize env and agent state
 env = SpaceEnvironment(grid=(20,20))
@@ -15,7 +16,7 @@ agent_state = {
 }
 
 game_over = False
-
+total_decision_time =0
 # game loop
 print("------------------start game-----------------------------------")
 print(f"starting position: {env.starting_position}")
@@ -34,7 +35,12 @@ while not game_over and env.timestep <200:
     #     action = "DOCK"
     # elif "COLLECT" in allowed_actions:
     #     action = "COLLECT"
+    start_time = time.time()
     action = random.choice(allowed_actions)
+    end_time = time.time()
+
+    total_decision_time += (end_time-start_time)
+    
     print(f"CHOSEN ACTION IS: {action}")
 
     # result
@@ -53,3 +59,21 @@ while not game_over and env.timestep <200:
     print(f"game status: {game_status}")
     print("--------------------------------------------------------")
     game_over = game_status["is_game_over"]
+
+# EVALUATION
+
+# timesteps
+total_timesteps = env.timestep
+
+# agent decision time
+avg_decision_time = total_decision_time / (total_timesteps+1)
+
+print(f"TOTAL TIMESTEPS: {total_timesteps}")
+print(f"TOTAL DECISION TIME: {total_decision_time}")
+print(f"AVERAGE DECISION TIME BY AGENT: {avg_decision_time}")
+
+# if agent achieved goal and how close he is from goal
+map_coverage = f"MAP COVERAGE GOAL:{env.mapping_goal_percentage} ACHIEVED: {agent_state['covered_map_percentage']}" if game_status['is_map_covered'] else f"MAP COVERAGE GOAL:{env.mapping_goal_percentage} WASN'T ACHIEVED: {agent_state['covered_map_percentage']}"
+resources_met = f"RESOURCES GOAL:{env.resource_goals} ACHIEVED: {agent_state['collected_resources']}" if game_status['is_resources_met'] else f"RESOURCES GOAL:{env.resource_goals} WASN'T ACHIEVED: {agent_state['collected_resources']}"
+print(map_coverage)
+print(resources_met)
